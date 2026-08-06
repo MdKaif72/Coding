15a4af2b1edad086c8fbcbf3bde576e05c19b923
MdKaifAnsari <mdkaif72098@gmail.com>
2026-08-06 06:36:08 +0530

    String Question

--- dsa/blind75Question/array/Main.java
+++ dsa/blind75Question/array/Main.java
@@ -1,5 +1,8 @@
 package dsa.blind75Question.array;
 
+import dsa.blind75Question.binary.SumOfInteger;
+import dsa.blind75Question.string.LongestSubstringWithoutRepeatingChr;
+
 import java.util.Arrays;
 
 public class Main {
@@ -7,6 +10,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Arrays.toString(TwoSum.twoSum(new int[]{3,3},6)));
+        System.out.println(BestTimeToBuyAndSell.maxProfit(new int[]{7,1,5,3,6,4}));
+        printArray(ProductOfArrayExceptItself.productExceptSelf(new int[]{1,2,3,4}));
+        System.out.println(SumOfInteger.getSum(9,111));
+        System.out.println(LongestSubstringWithoutRepeatingChr.lengthOfLongestSubstring("pwwkewxpw"));
 
+    }
+
+    public static void printArray(int []nums){
+        System.out.println(Arrays.toString(nums));
     }
 }
