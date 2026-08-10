41c02548c57cccabb80db284bbfd2cc24d329a36
MdKaifAnsari <mdkaif72098@gmail.com>
2026-08-10 22:06:22 +0530

    Tree Question to be solved

---
+++ dsa/blind75Question/tree/ConstructBTFromPREAndINOrder.java
@@ -0,0 +1,33 @@
+package dsa.blind75Question.tree;
+
+/*
+LC 105
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+
+
+ */
+
+public class ConstructBTFromPREAndINOrder {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+
+
+    }
+
+
+
+}
