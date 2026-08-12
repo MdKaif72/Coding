4f247c346559cccd50359fac192d248c24026d4a
MdKaifAnsari <mdkaif72098@gmail.com>
2026-08-12 16:20:55 +0530

    Heap Question solved

--- dsa/blind75Question/heap/FindMedianFromDataStream.java
+++ dsa/blind75Question/heap/FindMedianFromDataStream.java
@@ -43,6 +43,8 @@ Follow up:
 If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
 If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
 
+//max heap will contain a less value than or equal to  min heap value
+
  */
 
 import java.util.Comparator;
